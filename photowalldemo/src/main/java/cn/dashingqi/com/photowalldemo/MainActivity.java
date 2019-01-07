4f556ef812962f41a5a82dfc71bd8338d42a9a51
dashingqi <1415942241@qq.com>
2019-01-07 20:23:05 +0800

    PhotoWalls finished

--- photowalldemo/src/main/java/cn/dashingqi/com/photowalldemo/MainActivity.java
+++ photowalldemo/src/main/java/cn/dashingqi/com/photowalldemo/MainActivity.java
@@ -2,12 +2,55 @@ package cn.dashingqi.com.photowalldemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mImageThumbSizeSpacing;
+    private int mImageThumbSize;
+    private GridView mPhotoWall;
+    private PhotoWallAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageThumbSizeSpacing = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_spacing);
+        mImageThumbSize = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_size);
+        mPhotoWall = findViewById(R.id.mGridView);
+        mAdapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls, mPhotoWall);
+        mPhotoWall.setAdapter(mAdapter);
+        mPhotoWall.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                final int numColumns = (int) Math.floor(mPhotoWall
+                        .getWidth()
+                        / (mImageThumbSize + mImageThumbSizeSpacing));
+                if (numColumns > 0) {
+                    int columnWidth = (mPhotoWall.getWidth() / numColumns)
+                            - mImageThumbSizeSpacing;
+                    mAdapter.setItemHeight(columnWidth);
+                    mPhotoWall.getViewTreeObserver()
+                            .removeGlobalOnLayoutListener(this);
+                }
+
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdapter.flushCache();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //退出程序的时候取消所有的下载
+        mAdapter.cancelTask();
     }
 }
--- photowalldemo/src/main/java/cn/dashingqi/com/photowalldemo/PhotoWallAdapter.java
+++ photowalldemo/src/main/java/cn/dashingqi/com/photowalldemo/PhotoWallAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.annotation.NonNull;
@@ -21,12 +22,14 @@ import com.jakewharton.disklrucache.DiskLruCache;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.security.MessageDigest;
+import java.util.HashSet;
 
 /**
  * <p>文件描述：<p>
@@ -35,11 +38,12 @@ import java.security.MessageDigest;
  * <p>更改时间：2019/1/3<p>
  * <p>版本号：1<p>
  */
-public class PhotoWailAdapter extends ArrayAdapter<String> {
+public class PhotoWallAdapter extends ArrayAdapter<String> {
 
     private GridView mPhotoWall;
     private final LruCache<String, Bitmap> mMemoryCache;
     private DiskLruCache mDiskLruCache;
+    private HashSet<BitmapWorkerTask> taskCollection;
 
     /**
      * 记录每个子项的高度
@@ -49,14 +53,16 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
     private BufferedInputStream in;
     private BufferedOutputStream out;
 
-    public PhotoWailAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull String[] objects, GridView photoWall) {
-        super(context, resource, textViewResourceId, objects);
+    public PhotoWallAdapter(@NonNull Context context, int textViewResourceId, @NonNull String[] objects, GridView photoWall) {
+        super(context, textViewResourceId, objects);
         mPhotoWall = photoWall;
 
         //获取到应用程序最大的内存
         int maxMemory = (int) Runtime.getRuntime().maxMemory();
         //缓存内存为最大内存的1/8
         int cacheSize = maxMemory / 8;
+
+        taskCollection = new HashSet<>();
 
         mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
@@ -112,12 +118,34 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
         return mMemoryCache.get(key);
     }
 
+    /**
+     * 将一张图片存储到LruCache中
+     *
+     * @param key
+     * @param bitmap 这里传入从网络上下载的Bitmap对象
+     */
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemoryCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    /**
+     * 加载图片资源
+     *
+     * @param imageView
+     * @param imageUrl
+     */
     public void loadBitmaps(ImageView imageView, String imageUrl) {
         try {
 
             Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
             if (bitmap == null) {
                 //就开启异步任务去下载
+                BitmapWorkerTask task = new BitmapWorkerTask();
+                taskCollection.add(task);
+                task.execute(imageUrl);
+
             } else {
                 if (imageView != null && bitmap != null) {
                     imageView.setImageBitmap(bitmap);
@@ -131,7 +159,7 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
     }
 
     /**
-     * 根据传入的uniqueName获取到硬盘缓存的路径地址
+     * 获取硬盘缓存的路径
      *
      * @param context
      * @param name    用来确认缓存的类别
@@ -164,6 +192,19 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
         }
 
         return 1;
+    }
+
+    /**
+     * 设置Item子项的高度
+     *
+     * @param columnWidth
+     */
+    public void setItemHeight(int columnWidth) {
+        if (columnWidth == mItemHeight)
+            return;
+        mItemHeight = columnWidth;
+        notifyDataSetChanged();
+
     }
 
     /**
@@ -240,12 +281,90 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
     }
 
     /**
+     * 取消所有异步下载或者等待的任务
+     */
+    public void cancelTask() {
+        if (taskCollection != null) {
+            for (BitmapWorkerTask task : taskCollection) {
+                task.cancel(false);
+
+            }
+        }
+    }
+
+    /**
+     * 将缓存记录同步到journal文件中
+     */
+    public void flushCache() {
+        if (mDiskLruCache != null) {
+            try {
+                mDiskLruCache.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
      * 异步下载图片的任务
      */
-    class BitmapWorkerTask extends AsyncTask<String,Void,Bitmap>{
+    class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+
+        private FileInputStream fileInputStream;
+        private FileDescriptor fileDescriptor;
+        private String imageUrl;
 
         @Override
         protected Bitmap doInBackground(String... params) {
+            imageUrl = params[0];
+
+            try {
+                //先从磁盘中查找
+                String imageKey = hashKeyForDisk(imageUrl);
+                DiskLruCache.Snapshot snapshot = mDiskLruCache.get(imageKey);
+                if (snapshot == null) {
+                    DiskLruCache.Editor edit = mDiskLruCache.edit(imageKey);
+                    if (edit != null) {
+                        OutputStream outputStream = edit.newOutputStream(0);
+                        if (downLoadUrlToStream(imageUrl, outputStream)) {
+                            edit.commit();
+                        } else {
+                            edit.abort();
+                        }
+                    }
+
+                    snapshot = mDiskLruCache.get(imageKey);
+                }
+
+                if (snapshot != null) {
+                    fileInputStream = (FileInputStream) snapshot.getInputStream(0);
+                    fileDescriptor = fileInputStream.getFD();
+                }
+
+                //将缓存的数据解析成Bitmap
+                Bitmap bitmap = null;
+                if (fileDescriptor != null) {
+                    bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                }
+
+                if (bitmap != null) {
+                    //将Bitmap再次添加到内存当中
+                    addBitmapToMemoryCache(params[0], bitmap);
+                }
+
+                return bitmap;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fileDescriptor == null && fileInputStream != null) {
+                    try {
+                        fileInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
             return null;
         }
@@ -253,6 +372,15 @@ public class PhotoWailAdapter extends ArrayAdapter<String> {
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             super.onPostExecute(bitmap);
+            //根据Tag找到对应的ImageView，将下载好的图片显示出来
+
+            ImageView imageView = mPhotoWall.findViewWithTag(imageUrl);
+            if (imageView != null && bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+
+            taskCollection.remove(this);
+
         }
     }
 }
